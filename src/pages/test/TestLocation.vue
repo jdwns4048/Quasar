dbf656c79ae769910d16cbe662f845c035a78c1b
ZionYeom <yeomzion@gmail.com>
2024-10-07 14:56:47 +0900

    add - 주소찾기 페이지 추가.

---
+++ src/pages/test/TestLocation.vue
@@ -0,0 +1,30 @@
+<template>
+    <q-page class="q-pa-md">
+        <div>주소검색</div>
+    </q-page>
+</template>
+
+<script lang="ts">
+//import
+
+export default {
+    name: 'TestLocation',
+    components: {},
+    emits: [],
+    props: {},
+    setup() {
+        //-------- 상수 --------
+
+        //-------- 변수 --------
+
+        //-------- 함수 --------
+
+        //-------- 이벤트 핸들러 --------
+
+        //-------- watch, watchEffect --------
+
+        //-------- 라이프 사이클 --------
+        return {};
+    }
+};
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,29 +1,30 @@
-import { RouteRecordRaw } from 'vue-router';
+import {RouteRecordRaw} from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'sales-activity', component: () => import('pages/sales/SalesActivityMain.vue') },
-      { path: 'dashboard', component: () => import('pages/dashboard/orderSalesMain.vue') },
-      { path: 'company-info', component: () => import('pages/company/CompanyInfoMain.vue') },
-      { path: 'company-contact', component: () => import('pages/company/ContactMain.vue') },
-      { path: 'autocomplete', component: () => import('pages/test/Autocomplete.vue') },
-      { path: 'selectbox', component: () => import('pages/test/SelectBox.vue') },
-      { path: 'combobox', component: () => import('pages/test/Combobox.vue') },
-      { path: 'inputvalue', component: () => import('pages/test/InputValue.vue') },
-      { path: 'validator', component: () => import('pages/test/Validator.vue') }
-    ],
-  },
+    {
+        path: '/',
+        component: () => import('layouts/MainLayout.vue'),
+        children: [
+            {path: '', component: () => import('pages/IndexPage.vue')},
+            {path: 'sales-activity', component: () => import('pages/sales/SalesActivityMain.vue')},
+            {path: 'dashboard', component: () => import('pages/dashboard/orderSalesMain.vue')},
+            {path: 'company-info', component: () => import('pages/company/CompanyInfoMain.vue')},
+            {path: 'company-contact', component: () => import('pages/company/ContactMain.vue')},
+            {path: 'autocomplete', component: () => import('pages/test/Autocomplete.vue')},
+            {path: 'selectbox', component: () => import('pages/test/SelectBox.vue')},
+            {path: 'combobox', component: () => import('pages/test/Combobox.vue')},
+            {path: 'inputvalue', component: () => import('pages/test/InputValue.vue')},
+            {path: 'validator', component: () => import('pages/test/Validator.vue')},
+            {path: 'location', component: () => import('pages/test/TestLocation.vue')}
+        ]
+    },
 
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
-  },
+    // Always leave this as last one,
+    // but you can also remove it
+    {
+        path: '/:catchAll(.*)*',
+        component: () => import('pages/ErrorNotFound.vue')
+    }
 ];
 
 export default routes;
