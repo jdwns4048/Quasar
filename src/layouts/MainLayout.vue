18ccec1c0836ab9face60d4c192b48e49506e569
ZionYeom <yeomzion@gmail.com>
2024-10-07 14:57:04 +0900

    fix - 메인페이지 테스트에 맞도록 수정.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,183 +1,146 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+    <q-layout view="lHh Lpr lFf">
+        <q-header elevated>
+            <q-toolbar>
+                <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title class="text-center full-width" clickable @click="changeUrl('/')">
-          T-CRM
-        </q-toolbar-title>
+                <q-toolbar-title class="text-center full-width" clickable @click="changeUrl('/')"> T-CRM </q-toolbar-title>
 
-        <div><q-icon name="notifications" style="font-size: 24px"/></div>
-      </q-toolbar>
-    </q-header>
+                <div><q-icon name="notifications" style="font-size: 24px" /></div>
+            </q-toolbar>
+        </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          <div style="font-size: 18px; font-weight: bold; color: #1976D2">Quick Menu</div>
-        </q-item-label>
-        <q-item clickable @click="changeUrl('/')">
-          <q-icon name="home" style="font-size: 24px; margin-right: 15px" />
-          <q-item-section>
-            Home
-          </q-item-section>
-        </q-item>
-        <q-item clickable @click="changeUrl('/sales-activity')">
-          <q-icon name="calendar_month" style="font-size: 24px; margin-right: 15px" />
-          <q-item-section>
-            영업활동관리
-          </q-item-section>
-        </q-item>
-        <q-item clickable @click="changeUrl('/dashboard')">
-          <q-icon name="bar_chart" style="font-size: 24px; margin-right: 15px" />
-          <q-item-section>
-            수주판매현황
-          </q-item-section>
-        </q-item>
-        <q-item clickable @click="changeUrl('/company-info')">
-          <q-icon name="emoji_transportation" style="font-size: 24px; margin-right: 15px" />
-          <q-item-section>
-            고객정보
-          </q-item-section>
-        </q-item>
-        <q-item clickable @click="changeUrl('/company-contact')">
-          <q-icon name="diversity_3" style="font-size: 24px; margin-right: 15px" />
-          <q-item-section>
-            고객담당자정보
-          </q-item-section>
-        </q-item>
+        <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+            <q-list>
+                <q-item-label header>
+                    <div style="font-size: 18px; font-weight: bold; color: #1976d2">Quick Menu</div>
+                </q-item-label>
+                <q-item clickable @click="changeUrl('/')">
+                    <q-icon name="home" style="font-size: 24px; margin-right: 15px" />
+                    <q-item-section> Home </q-item-section>
+                </q-item>
+                <q-item clickable @click="changeUrl('/sales-activity')">
+                    <q-icon name="calendar_month" style="font-size: 24px; margin-right: 15px" />
+                    <q-item-section> 영업활동관리 </q-item-section>
+                </q-item>
+                <q-item clickable @click="changeUrl('/dashboard')">
+                    <q-icon name="bar_chart" style="font-size: 24px; margin-right: 15px" />
+                    <q-item-section> 수주판매현황 </q-item-section>
+                </q-item>
+                <q-item clickable @click="changeUrl('/company-info')">
+                    <q-icon name="emoji_transportation" style="font-size: 24px; margin-right: 15px" />
+                    <q-item-section> 고객정보 </q-item-section>
+                </q-item>
+                <q-item clickable @click="changeUrl('/company-contact')">
+                    <q-icon name="diversity_3" style="font-size: 24px; margin-right: 15px" />
+                    <q-item-section> 고객담당자정보 </q-item-section>
+                </q-item>
 
-        <!-- TODO 아래의 테스트를 하위 메뉴로 이동시킬것.       -->
-        <q-item clickable @click="changeUrl('/autocomplete')">
-          <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />
-          <q-item-section>
-            Autocomplete
-          </q-item-section>
-        </q-item>
-        <q-item clickable @click="changeUrl('/selectbox')">
-          <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />
-          <q-item-section>
-            Selectbox
-          </q-item-section>
-        </q-item>
-        <q-item clickable @click="changeUrl('/Combobox')">
-          <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />
-          <q-item-section>
-            Combobox
-          </q-item-section>
-        </q-item>
-        <q-item clickable @click="changeUrl('/InputValue')">
-          <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />
-          <q-item-section>
-            InputValue
-          </q-item-section>
-        </q-item>
-<!--        <q-item clickable @click="changeUrl('/Validator')">-->
-<!--          <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />-->
-<!--          <q-item-section>-->
-<!--            Validate-->
-<!--          </q-item-section>-->
-<!--        </q-item>-->
-        <br/>
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+                <!-- TODO 아래의 테스트를 하위 메뉴로 이동시킬것.       -->
+                <q-item clickable @click="changeUrl('/autocomplete')">
+                    <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />
+                    <q-item-section> Autocomplete </q-item-section>
+                </q-item>
+                <q-item clickable @click="changeUrl('/selectbox')">
+                    <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />
+                    <q-item-section> Selectbox </q-item-section>
+                </q-item>
+                <q-item clickable @click="changeUrl('/Combobox')">
+                    <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />
+                    <q-item-section> Combobox </q-item-section>
+                </q-item>
+                <q-item clickable @click="changeUrl('/InputValue')">
+                    <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />
+                    <q-item-section> InputValue </q-item-section>
+                </q-item>
+                <!--        <q-item clickable @click="changeUrl('/Validator')">-->
+                <!--          <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />-->
+                <!--          <q-item-section>-->
+                <!--            Validate-->
+                <!--          </q-item-section>-->
+                <!--        </q-item>-->
+                <br />
+                <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
+            </q-list>
+        </q-drawer>
 
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+        <q-page-container>
+            <router-view />
+        </q-page-container>
+    </q-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue';
-import { useRouter } from 'vue-router';
+import {defineComponent, ref} from 'vue';
+import EssentialLink, {EssentialLinkProps} from 'components/EssentialLink.vue';
+import {useRouter} from 'vue-router';
 
 const linksList: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  // {
-  //   title: 'Github',
-  //   caption: 'github.com/quasarframework',
-  //   icon: 'code',
-  //   link: 'https://github.com/quasarframework'
-  // },
-  // {
-  //   title: 'Discord Chat Channel',
-  //   caption: 'chat.quasar.dev',
-  //   icon: 'chat',
-  //   link: 'https://chat.quasar.dev'
-  // },
-  // {
-  //   title: 'Forum',
-  //   caption: 'forum.quasar.dev',
-  //   icon: 'record_voice_over',
-  //   link: 'https://forum.quasar.dev'
-  // },
-  // {
-  //   title: 'Twitter',
-  //   caption: '@quasarframework',
-  //   icon: 'rss_feed',
-  //   link: 'https://twitter.quasar.dev'
-  // },
-  // {
-  //   title: 'Facebook',
-  //   caption: '@QuasarFramework',
-  //   icon: 'public',
-  //   link: 'https://facebook.quasar.dev'
-  // },
-  // {
-  //   title: 'Quasar Awesome',
-  //   caption: 'Community Quasar projects',
-  //   icon: 'favorite',
-  //   link: 'https://awesome.quasar.dev'
-  // }
+    {
+        title: 'Docs',
+        caption: 'quasar.dev',
+        icon: 'school',
+        link: 'https://quasar.dev'
+    }
+    // {
+    //   title: 'Github',
+    //   caption: 'github.com/quasarframework',
+    //   icon: 'code',
+    //   link: 'https://github.com/quasarframework'
+    // },
+    // {
+    //   title: 'Discord Chat Channel',
+    //   caption: 'chat.quasar.dev',
+    //   icon: 'chat',
+    //   link: 'https://chat.quasar.dev'
+    // },
+    // {
+    //   title: 'Forum',
+    //   caption: 'forum.quasar.dev',
+    //   icon: 'record_voice_over',
+    //   link: 'https://forum.quasar.dev'
+    // },
+    // {
+    //   title: 'Twitter',
+    //   caption: '@quasarframework',
+    //   icon: 'rss_feed',
+    //   link: 'https://twitter.quasar.dev'
+    // },
+    // {
+    //   title: 'Facebook',
+    //   caption: '@QuasarFramework',
+    //   icon: 'public',
+    //   link: 'https://facebook.quasar.dev'
+    // },
+    // {
+    //   title: 'Quasar Awesome',
+    //   caption: 'Community Quasar projects',
+    //   icon: 'favorite',
+    //   link: 'https://awesome.quasar.dev'
+    // }
 ];
 
 export default defineComponent({
-  name: 'MainLayout',
+    name: 'MainLayout',
 
-  components: {
-    EssentialLink
-  },
+    components: {
+        EssentialLink
+    },
 
-  setup () {
-    const leftDrawerOpen = ref(false);
-    const router = useRouter();
-    function changeUrl(newUrl: string) {
-      router.push(newUrl);
+    setup() {
+        const leftDrawerOpen = ref(false);
+        const router = useRouter();
+        function changeUrl(newUrl: string) {
+            router.push(newUrl);
+        }
+        return {
+            linksList,
+            leftDrawerOpen,
+            toggleLeftDrawer() {
+                leftDrawerOpen.value = !leftDrawerOpen.value;
+            },
+            changeUrl
+        };
     }
-    return {
-      linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
-      },
-      changeUrl
-    };
-  }
 });
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,31 +1,18 @@
 <template>
     <q-page class="q-pa-md">
-        <div style="margin-bottom: 20px; font-size: 18px; font-weight: bold; color: #1976d2">모바일 업무 지원</div>
-        <q-select use-input use-chips v-model="searchClientInfo" :options="clientList" input-debounce="0" hint="2글자 이상 검색해주세요" label="고객/담당자 조회" behavior="menu" style="margin-bottom: 20px" @filter="minCharFilter">
-            <q-icon name="search" size="30px" />
-            <template v-slot:no-option>
-                <q-item>
-                    <q-item-section class="text-grey"> No results </q-item-section>
-                </q-item>
-            </template>
-        </q-select>
-        <div class="row items-center justify-center q-gutter-lg">
-            <q-btn size="md" color="white" text-color="black" stack style="min-width: 130px" @click="changeUrl('/sales-activity')">
-                <q-icon name="calendar_month" style="font-size: 40px" />
-                <div style="margin-top: 10px; font-size: 15px">영업활동관리</div>
+        <div class="column items-center justify-evenly q-gutter-lg">
+            <q-btn class="item-button" size="md" color="white" text-color="black" stack @click="changeUrl('/sales-activity')">
+                <q-icon class="button-icon" name="calendar_month" />
+                <div>Calendar</div>
             </q-btn>
 
-            <q-btn size="md" color="white" text-color="black" stack style="min-width: 130px" @click="changeUrl('/dashboard')">
-                <q-icon name="bar_chart" style="font-size: 40px" />
-                <div style="margin-top: 10px; font-size: 15px">수주판매현황</div>
+            <q-btn class="item-button" size="md" color="white" text-color="black" stack @click="changeUrl('/dashboard')">
+                <q-icon class="button-icon" name="pin_drop" />
+                <div>지도</div>
             </q-btn>
-            <q-btn size="md" color="white" text-color="black" stack style="min-width: 130px" @click="changeUrl('/company-info')">
-                <q-icon name="emoji_transportation" style="font-size: 40px" />
-                <div style="margin-top: 10px; font-size: 15px">고객정보</div>
-            </q-btn>
-            <q-btn size="md" color="white" text-color="black" stack style="min-width: 130px" @click="changeUrl('/company-contact')">
-                <q-icon name="diversity_3" style="font-size: 40px" />
-                <div style="margin-top: 10px; font-size: 13px">고객담당자정보</div>
+            <q-btn class="item-button" size="md" color="white" text-color="black" stack @click="changeUrl('/location')">
+                <q-icon class="button-icon" name="my_location" />
+                <div>주소 검색</div>
             </q-btn>
         </div>
     </q-page>
@@ -65,8 +52,26 @@ export default defineComponent({
             searchClientInfo,
             clientList,
             changeUrl,
-            minCharFilter,
+            minCharFilter
         };
     }
 });
 </script>
+<style scoped>
+.q-pa-md {
+    display: flex;
+    justify-content: center;
+}
+.item-button {
+    min-width: 130px;
+}
+
+.button-icon {
+    font-size: 40px;
+}
+
+.item-button > div {
+    margin-top: 10px;
+    font-size: 13px;
+}
+</style>
