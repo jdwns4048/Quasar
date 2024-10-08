e50e938d2635bde4a99eb64e5d326a1ea412c547
younsungjune <tjdwns4048@xenoimpact.com>
2024-10-08 17:38:31 +0900

    fix - 달력 페이지(3)

--- src/pages/test/TestCalendar.vue
+++ src/pages/test/TestCalendar.vue
@@ -12,16 +12,42 @@ import Interaction from '@event-calendar/interaction';
 
 type CalendarType = 'dayGridMonth' | 'timeGridWeek' | 'timeGridDay' | 'listWeek';
 type Type = 'month' | 'week';
+export type CalendarEvent<T extends Record<string, any> = Record<string, any>> = {
+  id: string;
+  start: string; //ISO8601
+  end: string; //ISO8601
+  resourceId?: string | number;
+  extendedProps: T;
+  allDay?: boolean;
+  title: string;
+  display?: 'auto' | 'background' | 'ghost' | 'preview' | 'pointer';
+};
+
+export type CalendarView = {
+  activeEnd: Date;
+  activeStart: Date;
+  calendar: any;
+  currentEnd: Date;
+  currentStart: Date;
+  title: string;
+  type: CalendarType;
+};
+
+type CalendarEventContent = {
+  event: CalendarEvent;
+  timeText: string;
+  view: CalendarView;
+};
 const CalendarTypeMap: Record<Type, CalendarType> = {month: 'dayGridMonth', week: 'listWeek'};
 const DEFAULT_FORMAT = 'YYYY-MM-DD HH:mm';
 
 export default defineComponent({
     name: 'TestCalendar',
-    props:{
-      type: {
-        type: String as PropType<Type>,
-        default: 'month'
-      },
+    props: {
+        type: {
+            type: String as PropType<Type>,
+            default: 'month'
+        }
     },
     setup(props, context) {
         const container = ref<HTMLElement>();
@@ -36,10 +62,35 @@ export default defineComponent({
                 props: {
                     plugins: [TimeGrid, DayGrid, List, Interaction],
                     options: {
-                        view: typeToCalendarType(props.type)
+                        view: typeToCalendarType(props.type),
+                        headerToolbar: {
+                            start: 'prev,today,next',
+                            center: 'title',
+                            end: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+                        },
+                        buttonText: {
+                            dayGridMonth: 'Month',
+                            timeGridWeek: 'Week',
+                            timeGridDay: 'Day',
+                            listWeek: 'List',
+                            today: 'Today'
+                        },
+                        // events: eventItems.value
+                        // date: model.value,
+                      dayMaxEvents: true,
+                      nowIndicator: true,
+                      selectable: false,
+                      eventStartEditable: false, // 드래그 가능 여부
+                      eventDurationEditable: false, // 크기 조정 여부
+                      eventContent(info: CalendarEventContent){
+                        // const item = info.event;
+                        // const instance = slotToComponent(context.slots.default(item));
+                        // return {domNodes: [instance.el]};
+                      }
                     }
                 }
             });
+          instance.value.refetchEvents();
         }
         function typeToCalendarType(value: Type): CalendarType {
             return CalendarTypeMap[value];
