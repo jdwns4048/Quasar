8b74d534d7966e78b9c4823f93e197e8c4b98816
ZionYeom <yeomzion@gmail.com>
2024-10-08 10:41:14 +0900

    fix - 메인페이지 미사용 탭 주석처리.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -21,50 +21,40 @@
                 </q-item>
                 <q-item clickable @click="changeUrl('/sales-activity')">
                     <q-icon name="calendar_month" style="font-size: 24px; margin-right: 15px" />
-                    <q-item-section> 영업활동관리 </q-item-section>
+                    <q-item-section> Calendar </q-item-section>
                 </q-item>
                 <q-item clickable @click="changeUrl('/dashboard')">
                     <q-icon name="bar_chart" style="font-size: 24px; margin-right: 15px" />
-                    <q-item-section> 수주판매현황 </q-item-section>
+                    <q-item-section> 지도 </q-item-section>
                 </q-item>
-                <q-item clickable @click="changeUrl('/company-info')">
+                <q-item clickable @click="changeUrl('/location')">
                     <q-icon name="emoji_transportation" style="font-size: 24px; margin-right: 15px" />
-                    <q-item-section> 고객정보 </q-item-section>
-                </q-item>
-                <q-item clickable @click="changeUrl('/company-contact')">
-                    <q-icon name="diversity_3" style="font-size: 24px; margin-right: 15px" />
-                    <q-item-section> 고객담당자정보 </q-item-section>
+                    <q-item-section> 주소 검색 </q-item-section>
                 </q-item>
 
                 <!-- TODO 아래의 테스트를 하위 메뉴로 이동시킬것.       -->
-                <q-item clickable @click="changeUrl('/autocomplete')">
-                    <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />
-                    <q-item-section> Autocomplete </q-item-section>
-                </q-item>
-                <q-item clickable @click="changeUrl('/selectbox')">
-                    <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />
-                    <q-item-section> Selectbox </q-item-section>
-                </q-item>
-                <q-item clickable @click="changeUrl('/Combobox')">
-                    <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />
-                    <q-item-section> Combobox </q-item-section>
-                </q-item>
-                <q-item clickable @click="changeUrl('/InputValue')">
-                    <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />
-                    <q-item-section> InputValue </q-item-section>
-                </q-item>
-                <!--        <q-item clickable @click="changeUrl('/Validator')">-->
-                <!--          <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />-->
-                <!--          <q-item-section>-->
-                <!--            Validate-->
-                <!--          </q-item-section>-->
-                <!--        </q-item>-->
-                <br />
+                <!--                <q-item clickable @click="changeUrl('/autocomplete')">-->
+                <!--                    <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />-->
+                <!--                    <q-item-section> Autocomplete </q-item-section>-->
+                <!--                </q-item>-->
+                <!--                <q-item clickable @click="changeUrl('/selectbox')">-->
+                <!--                    <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />-->
+                <!--                    <q-item-section> Selectbox </q-item-section>-->
+                <!--                </q-item>-->
+                <!--                <q-item clickable @click="changeUrl('/Combobox')">-->
+                <!--                    <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />-->
+                <!--                    <q-item-section> Combobox </q-item-section>-->
+                <!--                </q-item>-->
+                <!--                <q-item clickable @click="changeUrl('/InputValue')">-->
+                <!--                    <q-icon name="attachment" style="font-size: 24px; margin-right: 15px" />-->
+                <!--                    <q-item-section> InputValue </q-item-section>-->
+                <!--                </q-item>-->
+                <!--                <br />-->
                 <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
             </q-list>
         </q-drawer>
 
-        <q-page-container>
+        <q-page-container padding>
             <router-view />
         </q-page-container>
     </q-layout>
@@ -82,42 +72,6 @@ const linksList: EssentialLinkProps[] = [
         icon: 'school',
         link: 'https://quasar.dev'
     }
-    // {
-    //   title: 'Github',
-    //   caption: 'github.com/quasarframework',
-    //   icon: 'code',
-    //   link: 'https://github.com/quasarframework'
-    // },
-    // {
-    //   title: 'Discord Chat Channel',
-    //   caption: 'chat.quasar.dev',
-    //   icon: 'chat',
-    //   link: 'https://chat.quasar.dev'
-    // },
-    // {
-    //   title: 'Forum',
-    //   caption: 'forum.quasar.dev',
-    //   icon: 'record_voice_over',
-    //   link: 'https://forum.quasar.dev'
-    // },
-    // {
-    //   title: 'Twitter',
-    //   caption: '@quasarframework',
-    //   icon: 'rss_feed',
-    //   link: 'https://twitter.quasar.dev'
-    // },
-    // {
-    //   title: 'Facebook',
-    //   caption: '@QuasarFramework',
-    //   icon: 'public',
-    //   link: 'https://facebook.quasar.dev'
-    // },
-    // {
-    //   title: 'Quasar Awesome',
-    //   caption: 'Community Quasar projects',
-    //   icon: 'favorite',
-    //   link: 'https://awesome.quasar.dev'
-    // }
 ];
 
 export default defineComponent({
