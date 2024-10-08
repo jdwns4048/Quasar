2d084274f871a79e85390a2f57bbcfe2a6fa147a
younsungjune <tjdwns4048@xenoimpact.com>
2024-10-08 17:06:42 +0900

    fix - 달력 페이지(1)

--- src/pages/test/TestCalendar.vue
+++ src/pages/test/TestCalendar.vue
@@ -1,31 +1,47 @@
 <template>
-  <div id="calendar"></div>
+    <div id="calendar" ref="container"></div>
 </template>
 
-<script setup>
-import { onMounted } from 'vue';
+<script lang="ts">
+import {onMounted, defineComponent, ref} from 'vue';
 import Calendar from '@event-calendar/core';
 import TimeGrid from '@event-calendar/time-grid';
+import DayGrid from '@event-calendar/day-grid';
+import List from '@event-calendar/list';
+import Interaction from '@event-calendar/interaction';
 
-onMounted(() => {
-  const calendar = new Calendar({
-    target: document.getElementById('calendar'),
-    props: {
-      plugins: [TimeGrid],
-      options: {
-        view: 'timeGridWeek',
-        events: [
-          // { id: '1', title: 'Event 1', start: '2024-10-15T10:00:00' },
-          // { id: '2', title: 'Event 2', start: '2024-10-16T14:00:00' }
-        ]
-      }
+export default defineComponent({
+    name: 'TestCalendar',
+    setup() {
+        const container = ref<HTMLElement>();
+        const instance = ref<Calendar>();
+        onMounted(() => {
+          initialize();
+        });
+        function initialize(){
+            instance.value = new Calendar({
+              target: container.value,
+              props:{
+                plugins: [TimeGrid, DayGrid, List, Interaction],
+                options:{
+                  view: 'timeGridWeek',
+                  events: [
+                    // { id: '1', title: 'Event 1', start: '2024-10-15T10:00:00' },
+                    // { id: '2', title: 'Event 2', start: '2024-10-16T14:00:00' }
+                  ]
+                }
+              }
+            })
+        }
+        return{
+          container
+        }
     }
-  });
 });
 </script>
 
 <style scoped>
 #calendar {
-  padding: 16px;
+    padding: 16px;
 }
 </style>
