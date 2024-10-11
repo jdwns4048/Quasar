7035e9c78f2aa7cb78698ba440d977ab293dc0e8
ZionYeom <yeomzion@gmail.com>
2024-10-11 12:16:35 +0900

    fix - 주소찾기 팝업 open/close 방식 변경.

--- src/pages/address/PostcodeDialog.vue
+++ src/pages/address/PostcodeDialog.vue
@@ -4,7 +4,7 @@
             <q-card-section class="test-cardSection">
                 <div class="dialogHeader">
                     <span class="text-h6">주소 검색</span>
-                    <q-btn class="bg-white text-primary closeBtn" v-close-popup> 닫기 </q-btn>
+                    <q-btn class="bg-white text-primary closeBtn" @click="close"> 닫기 </q-btn>
                 </div>
                 <div ref="postcodeWrap" class="postcode-iframe-wrap"></div>
                 <div class="guide"></div>
@@ -17,46 +17,25 @@
 import {ref, onMounted, watch, nextTick} from 'vue';
 import {Address} from 'src/defines/address';
 
-const props = defineProps({
-    modelValue: Boolean,
-    onComplete: Function
-});
-
-const emit = defineEmits(['update:modelValue', 'onComplete']);
+const emit = defineEmits(['apply']);
 
 const POSTCODE_WIDTH = '100%';
 const POSTCODE_HEIGHT = '60%';
-const isOpened = ref(props.modelValue);
+const isOpened = ref(false);
 const postcodeWrap = ref(null); //Postcode 렌더링할 DOM 요소
-
-watch(
-    () => props.modelValue,
-    newVal => {
-        isOpened.value = newVal;
-    }
-);
-
-watch(isOpened, newVal => {
-    emit('update:modelValue', newVal); // isOpened 상태값 변경시 상위 Component로 전달
-
-    if (newVal) {
-        nextTick(() => {
-            embedPostcode();
-        });
-    }
-});
+const isPostcodeScriptLoaded = ref(true);
 
 /**
- * iframe을 통해 카카오 우편번호 서비스 API를 임베딩
+ * iframe을 통해 다이얼로그(팝업) 표시
  */
 const embedPostcode = () => {
     new (window as any).daum.Postcode({
-        onComplete: (data: Address) => {
-            emit('onComplete', data); // 콜백으로 처리
-            isOpened.value = false; // 주소 선택 후 다이얼로그 닫기
-        },
         width: POSTCODE_WIDTH,
-        height: POSTCODE_HEIGHT
+        height: POSTCODE_HEIGHT,
+        onComplete: (data: Address) => {
+            emit('apply', data);
+            close(); // 주소 선택 후 다이얼로그 닫기
+        }
     }).embed(postcodeWrap.value!);
 };
 
@@ -64,16 +43,36 @@ const embedPostcode = () => {
  * 카카오 우편번호 서비스 API 스크립트를 로드
  */
 const loadPostcode = () => {
-    const script = document.createElement('script');
-    script.src = '//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js';
-    script.async = true;
-    script.onload = embedPostcode;
-    document.head.appendChild(script);
+    if (isPostcodeScriptLoaded.value) {
+        const script = document.createElement('script');
+        script.src = '//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js';
+        script.async = true;
+        script.onload = () => {
+            isPostcodeScriptLoaded.value = false;
+            embedPostcode();
+        };
+        document.head.appendChild(script);
+    } else {
+        embedPostcode();
+    }
 };
+
+function open() {
+    isOpened.value = true;
+    nextTick(() => {
+        embedPostcode();
+    });
+}
+
+function close() {
+    isOpened.value = false;
+}
 
 onMounted(() => {
     loadPostcode();
 });
+
+defineExpose({open, close});
 </script>
 
 <style scoped>
--- src/pages/address/TestLocation.vue
+++ src/pages/address/TestLocation.vue
@@ -1,7 +1,7 @@
 <template>
     <q-page class="q-pa-md">
-        <q-btn color="primary" label="주소 검색" @click="isOpened = true" />
-        <PostcodeDialog v-model="isOpened" @onComplete="getAddress" />
+        <q-btn color="primary" label="주소 검색" @click="openPopup" />
+        <PostcodeDialog ref="popup" @apply="getAddress" />
         <q-input class="textArea" v-model="address" label="response data" filled type="textarea" readonly rows="25" />
     </q-page>
 </template>
@@ -12,16 +12,20 @@ import PostcodeDialog from 'src/pages/address/PostcodeDialog.vue';
 import {formatAddress} from 'src/plugin/utils/formatAddress';
 import {Address} from 'src/defines/address';
 
-const isOpened = ref(false);
 const address = ref('');
+const popup = ref(PostcodeDialog);
 
 /**
- * 카카오 우편번호 서비스 결과 값을 보기 좋은 형태로  변환.
+ * 카카오 우편번호 서비스 결과 값을 보기 좋은 형태로 변환.
  * @param data
  */
 const getAddress = (data: Address) => {
     address.value = formatAddress(data);
 };
+
+function openPopup() {
+    popup.value.open();
+}
 </script>
 
 <style>
