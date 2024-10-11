1cc79151b1f169cc062cc27d0ac19d154dcb7416
jeonghyun135 <jeonghyun135@xenoimpact.com>
2024-10-11 14:09:48 +0900

    fix - map 마커 수정

--- src/pages/map/MapView.vue
+++ src/pages/map/MapView.vue
@@ -1,140 +1,123 @@
 <template>
-    <q-layout view="lHh Lpr lFf">
-        <q-page-container class="row q-gutter-sm">
-            <div id="map" @click="mapClick"></div>
-            <q-page class="q-pa-md">
-                <q-btn color="primary" label="주소 검색" @click="isOpened = true" />
-                <PostcodeDialog v-model="isOpened" @onComplete="getAddress" />
-                <q-input class="textarea" v-model="centerAddr" label="map data" filled type="textarea" />
-            </q-page>
-        </q-page-container>
-    </q-layout>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container class="q-gutter-sm">
+      <q-page class="q-pa-md">
+        <div id="map" @click="mapClick"></div>
+        <q-btn class="q-mt-md" color="primary" label="주소 검색" @click="openPopup"/>
+        <PostcodeDialog ref="popup" @apply="getAddress"/>
+        <q-input class="textarea" v-model="centerAddr" label="map data" filled type="textarea"/>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 
-<script lang="ts">
-import {defineComponent, onMounted, ref} from 'vue';
+<script setup lang="ts">
+import {onMounted, ref} from 'vue';
 import PostcodeDialog from 'pages/address/PostcodeDialog.vue';
 import {Address} from 'src/defines/address';
 
-export default defineComponent({
-    name: 'KakaoMap',
-    components: {PostcodeDialog},
-    setup() {
-        const map = ref<null | kakao.maps.Map>(null);
-        const marker = ref<null | kakao.maps.Marker>(null);
-        const geocoder = ref<any>(null);
-        const centerAddr = ref('');
-        const isOpened = ref(false);
+const map = ref<kakao.maps.Map>();
+const marker = ref<kakao.maps.Marker>();
+const geocoder = ref<any>(null);
+const centerAddr = ref('');
+const popup = ref(PostcodeDialog);
 
-        function initMap() {
-            const container = document.getElementById('map');
-            const options = {
-                center: new kakao.maps.LatLng(37.2312, 127.071),
-                level: 5
-            };
-            const markerPosition = options.center;
-            map.value = new kakao.maps.Map(container as HTMLElement, options);
-            geocoder.value = new kakao.maps.services.Geocoder();
-            marker.value = new kakao.maps.Marker({
-                map: map.value,
-                position: markerPosition
-            });
-        }
-        /**
-         * 선택된 주소의 좌표 해당 위치로 지도 중심 이동 및 마커 이동
-         * @param point
-         */
-        function searchAddress(point) {
-            if (map.value) {
-                marker.value = new kakao.maps.Marker({
-                    map: map.value,
-                    position: point
-                });
-            }
-        }
+function initMap() {
+  const container = document.getElementById('map');
+  const options = {
+    center: new kakao.maps.LatLng(37.2312, 127.0710),
+    level: 5,
+  };
+  const markerPosition = options.center;
+  map.value = new kakao.maps.Map(container as HTMLElement, options);
+  geocoder.value = new kakao.maps.services.Geocoder();
+  marker.value = new kakao.maps.Marker({
+    map: map.value,
+    position: markerPosition
+  });
 
-        /**
-         * 검색 결과 항목 클릭 시 지도 및 장소 정보 출력
-         * @param places
-         * @param callback
-         */
-        function displayPlaces(places, callback: Function) {
-            geocoder.value.addressSearch(places, callback);
-        }
+}
 
-        function mapClick() {
-            if (map.value) {
-                kakao.maps.event.addListener(map.value, 'click', function (position) {
-                    const coords = position.latLng;
-                    geocoder.value.coord2Address(coords.getLng(), coords.getLat(), searchDetailAddrFromCoords);
-                    searchAddress(coords);
-                });
-            } else {
-                console.error('Map is not initialized.');
-            }
-        }
+/**
+ * 선택된 주소의 좌표 해당 위치로 지도 중심 이동 및 마커 이동
+ * @param point
+ */
+function displayMarker(point) {
+  if (marker.value) {
+    marker.value.setMap(null);
+  }
+  marker.value = new kakao.maps.Marker({
+    map: map.value,
+    position: point
+  });
 
-        const searchDetailAddrFromCoords = function (result, status) {
-            if (status === kakao.maps.services.Status.OK) {
-                centerAddr.value = result[0].address.address_name;
-            } else {
-                alert('status 확인');
-            }
-        };
-        /**
-         * 카카오 우편번호 서비스 결과 값을 보기 좋은 형태로  변환.
-         * @param data
-         */
-        const getAddress = (data: Address) => {
-            centerAddr.value = data.address;
-            displayPlaces(centerAddr.value, function (result, status) {
-                if (status === kakao.maps.services.Status.OK) {
-                    const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-                    searchAddress(coords);
+}
 
-                    // 지도의 중심을 결과값으로 받은 위치로 이동
-                    map.value?.setCenter(coords);
-                }
-            });
-        };
+/**
+ * 검색 결과 항목 클릭 시 지도 및 장소 정보 출력
+ * @param places
+ * @param callback
+ */
+function displayPlaces(places, callback: Function) {
+  geocoder.value.addressSearch(places, callback)
+}
 
-        const loadMap = () => {
-            const script = document.createElement('script');
-            script.src = '//dapi.kakao.com/v2/maps/sdk.js?appkey=5de7bb5646496ded9485a7308b93b729&autoload=false&libraries=services';
-            script.onload = () => kakao.maps.load(initMap);
-            document.head.appendChild(script);
-        };
-        onMounted(() => {
-            loadMap();
-        });
 
-        return {
-            map,
-            marker,
-            geocoder,
-            centerAddr,
-            isOpened,
-            getAddress,
-            mapClick
-        };
+function mapClick() {
+  if (map.value) {
+    kakao.maps.event.addListener(map.value, 'click', function (position) {
+      const coords = position.latLng;
+      displayMarker(coords);
+      geocoder.value.coord2Address(coords.getLng(), coords.getLat(), searchDetailAddrFromCoords);
+    });
+  }
+
+}
+
+const searchDetailAddrFromCoords = function (result, status) {
+  if (status === kakao.maps.services.Status.OK) {
+    centerAddr.value = result[0].address.address_name;
+  }
+}
+/**
+ * 카카오 우편번호 서비스 결과 값을 보기 좋은 형태로  변환.
+ * @param data
+ */
+const getAddress = (data: Address) => {
+  centerAddr.value = data.address;
+  displayPlaces(centerAddr.value, function (result, status) {
+    if (status === kakao.maps.services.Status.OK) {
+      const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+      displayMarker(coords);
+      // 지도의 중심을 결과값으로 받은 위치로 이동
+      map.value?.setCenter(coords);
     }
+  });
+};
+
+function openPopup() {
+  popup.value.open();
+}
+
+const loadMap = () => {
+  const script = document.createElement('script');
+  script.src = '//dapi.kakao.com/v2/maps/sdk.js?appkey=5de7bb5646496ded9485a7308b93b729&autoload=false&libraries=services';
+  script.onload = () => kakao.maps.load(initMap);
+  document.head.appendChild(script);
+}
+onMounted(() => {
+  loadMap();
 });
+
 </script>
 
 <style scoped>
 #map {
-    width: 100%;
-    height: 400px;
-}
-.textarea {
-    margin-top: 20px;
+  width: 100%;
+  height: 400px;
 }
 
-.info_window__content {
-    width: 140px;
-    text-align: center;
-    font-size: 12px;
-    padding: 6px 0;
-    border-radius: 8px;
+.textarea {
+  margin-top: 20px;
 }
 </style>
