c4e330cc0b64cb4c28105926f3dcf42f879c7aba
younsungjune <tjdwns4048@xenoimpact.com>
2024-10-14 01:16:20 +0900

    Fix - 달력 편집 테스트 중

--- src/pages/calendar/CalendarEdit.vue
+++ src/pages/calendar/CalendarEdit.vue
@@ -1,31 +1,88 @@
 <template>
-    <div v-if="isVisible" style="background-color: blue">
-      <h6>Edit</h6>
-    </div>
+  <q-dialog v-model="isVisible" >
+    <q-card class="column full-height">
+      <q-card-section>
+        <q-input v-model="activityName" label="영업 활동" />
+        <!-- TODO Q-Date -->
+        <div style="display: flex">
+          <div class="q-pa-md">
+            <q-input filled v-model="startDate" mask="date" :rules="['date']" label="시작일">
+              <template v-slot:append>
+                <q-icon name="event" class="cursor-pointer">
+                  <q-menu cover transition-show="scale" transition-hide="scale">
+                    <q-date v-model="startDate">
+                      <div class="row items-center justify-end">
+                        <q-btn v-close-popup label="Close" color="primary" flat />
+                      </div>
+                    </q-date>
+                  </q-menu>
+                </q-icon>
+              </template>
+            </q-input>
+          </div>
+
+          <div class="q-pa-md">
+            <q-input filled v-model="endDate" mask="date" :rules="['date']" label="종료일">
+              <template v-slot:append>
+                <q-icon name="event" class="cursor-pointer">
+                  <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                    <q-date v-model="endDate">
+                      <div class="row items-center justify-end">
+                        <q-btn v-close-popup label="Close" color="primary" flat />
+                      </div>
+                    </q-date>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+          </div>
+        </div>
+
+      </q-card-section>
+      <q-card-actions align="right">
+        <q-btn flat label="닫기" @click="onClose" />
+        <q-btn flat label="저장" @click="onSave" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script lang="ts">
 import {defineComponent, ref} from 'vue';
 export default defineComponent({
-  name:'CalendarEdit',
-  setup(){
+  name: 'CalendarEdit',
+  setup() {
     const isVisible = ref(false);
-    function open(info){
+    const activityName = ref('');
+    const startDate = ref(null);
+    const endDate = ref(null);
+    function open(info) {
       console.log('Opening edit popup => ', info);
+      activityName.value = info.event.title;
+      startDate.value = info.event.start;
+      endDate.value = info.event.end;
       isVisible.value = true;
     }
-    function close(){
+
+    function onClose() {
+      activityName.value = '';
       isVisible.value = false;
     }
-    return{
-      isVisible,
-      open,
-      close
+    function onSave() {
+      isVisible.value = false;
     }
+    return {
+      isVisible,
+      activityName,
+      startDate,
+      endDate,
+      open,
+      onClose,
+      onSave
+    };
   }
-})
+});
 </script>
 
 <style scoped>
-
 </style>
