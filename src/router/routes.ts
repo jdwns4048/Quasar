7cdeab6ae49d2a938abd8ca1fb5e6f23077a2de2
younsungjune <tjdwns4048@xenoimpact.com>
2024-10-18 10:23:02 +0900

    Fix - Vue 라우터 방식 'history' 로 변환

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -13,15 +13,10 @@ const routes: RouteRecordRaw[] = [
             {path: 'validator', component: () => import('pages/test/Validator.vue')},
             {path: 'searchAddress', component: () => import('pages/searchAddress/SearchAddress.vue')},
             {path: 'map', component: () => import('pages/map/MapVIew.vue')},
-            //TODO 경로 수정할것 & 쿼리를 통해 Detail 로 보내줄것..
             {path: 'sales-activity', component: () => import('pages/sales/SalesActivityMain.vue')},
-
             {path: 'calendarDetail', component: () => import('pages/sales/SalesActivityDetail.vue')}
         ]
     },
-
-    // Always leave this as last one,
-    // but you can also remove it
     {
         path: '/:catchAll(.*)*',
         component: () => import('pages/ErrorNotFound.vue')
