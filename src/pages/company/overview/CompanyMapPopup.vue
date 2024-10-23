c1e26e964326a7197e9cbb688aeb40c545a0a7c6
ZionYeom <yeomzion@gmail.com>
2024-10-23 16:58:49 +0900

    add - 기본 화면(페이지) 컴포넌트 생성.

---
+++ src/pages/company/overview/CompanyMapPopup.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>위치보기</div>
+</template>
+
+<script setup lang="ts">
+//import
+
+// 상수 정의 (UPPER_SNAKE_CASE)
+
+// 변수 정의 (camelCase)
+
+//-------- watch, watchEffect --------
+
+// 함수 정의 (camelCase, 동사 + 명사 구조)
+
+// 이벤트 핸들러 정의 (on + 명사 + 동사 구조)
+
+//-------- 라이프 사이클 --------
+</script>
